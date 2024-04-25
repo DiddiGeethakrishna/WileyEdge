20424400217657502f6e63615d5fa6c461cc4cd3
DiddiGeethakrishna <70500217+DiddiGeethakrishna@users.noreply.github.com>
2024-04-25 15:14:28 +0530

    Add files via upload

---
+++ MultiThreading/src/Main.java
@@ -0,0 +1,11 @@
+//package threadsbasics;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println(Runtime.getRuntime().availableProcessors());
+
+    }
+
+}
